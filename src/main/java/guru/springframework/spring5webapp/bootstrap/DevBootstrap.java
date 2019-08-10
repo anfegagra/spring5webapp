70fd7aceb85f27ebba6234abac1fbd377a9ffc67
Andres Felipe García Granados <garandre@colpatria.com>
2019-08-10 16:53:08 -0500

    se crea componente para poblar la bd cuando inicie la aplicación

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.model.Author;
+import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+	AuthorRepository authorRepository;
+	BookRepository bookRepository;
+
+	public DevBootstrap(
+		AuthorRepository authorRepository, BookRepository bookRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+	}
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		initData();
+	}
+
+	public void initData() {
+
+		Author eric = new Author("Eric", "Evans");
+		Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+		eric.getBooks().add(ddd);
+		ddd.getAuthors().add(eric);
+
+		authorRepository.save(eric);
+		bookRepository.save(ddd);
+
+		Author rod = new Author("Rod", "Johnson");
+		Book noEJB = new Book("J2EE Development withoud EJB", "23444", "Worx");
+		rod.getBooks().add(noEJB);
+
+		authorRepository.save(rod);
+		bookRepository.save(noEJB);
+
+	}
+}
