603b30c5cf011c07b0dadfd3b1ce219a6957df85
Andres Felipe García Granados <garandre@colpatria.com>
2019-08-10 13:00:32 -0500

    se crean los repositorios para las entidades Author y Book

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+}
